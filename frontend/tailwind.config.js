6954bbfdeea3057364ab6dab529e82f74933a068
vikranth <vikmandadi@gmail.com>
2024-09-08 13:43:52 -0500

    styling set up

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,15 @@ module.exports = {
     "./<custom directory>/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      primary: "#181F1C",
+      secondary: "#274029",
+      tertiary: "#315C2B",
+      bg: "#fff",
+      font1: "#fff",
+      font2: "#9EA93F",
+    },
     extend: {},
   },
   plugins: [],
 };
-
